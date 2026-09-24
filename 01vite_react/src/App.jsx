41ef686f6a4ae80aca34b7cb84f4e1660d02f23b
Vikas <sharmavikas12909@gmail.com>
2026-09-24 17:18:30 +0530

    basics of the React/ react under the hood

---
+++ 01vite_react/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai aur React - with Vite | Vikas </h1>
+  )
+}
+
+export default App
---
+++ 01vite_react/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai (){
+    return <h1>Your chai is ready</h1>;
+}
+
+Chai();
+
+export default Chai;
