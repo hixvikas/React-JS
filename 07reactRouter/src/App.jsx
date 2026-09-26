24259b9f22ec347c71adc35f5bfd9d33edf34e89
Vikas <sharmavikas12909@gmail.com>
2026-09-26 23:38:55 +0530

    React router project

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+import Home from './components/Home/Home'
+
+function App() {
+
+  return (
+    <>
+    {/* <Header /> */}
+    <Footer />
+    <Home />
+    </>
+  )
+}
+
+export default App
